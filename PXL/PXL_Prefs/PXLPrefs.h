668c239426e6f754fe997adbf9e71eb65b9f853b
Turan <84424530+turannul@users.noreply.github.com>
2023-02-05 04:43:54 +0300

    Update PXLPrefs.h

--- PXL/PXL_Prefs/PXLPrefs.h
+++ PXL/PXL_Prefs/PXLPrefs.h
@@ -1,7 +1,9 @@
 #import <Preferences/PSListController.h>
 #import <Foundation/Foundation.h>
 #import "Functions/Turann.h"
-
+#import "SparkColourPickerUtils.h"
+//#import "$(THEOS)/vendor/lib/Alderis.framework/Headers/Alderis.h"
+//#import "/Users/turannul/theos/vendor/lib/Alderis.framework/Headers/Alderis.h"
 @interface PXLPrefs:Turann{}
 @property (nonatomic, retain) UIBarButtonItem *respringApplyButton;
 @property (nonatomic, retain) UIBarButtonItem *respringConfirmButton;
