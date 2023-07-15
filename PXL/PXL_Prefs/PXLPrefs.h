3b8998b4cd06f5b2d340c0eb37a0920dc29110a4
Turann_ <84424530+turannul@users.noreply.github.com>
2023-07-15 04:06:53 +0300

    Update PXLPrefs.h

--- PXL/PXL_Prefs/PXLPrefs.h
+++ PXL/PXL_Prefs/PXLPrefs.h
@@ -2,7 +2,8 @@
 #import <Foundation/Foundation.h>
 #import "Functions/Turann.h"
 #import "Functions/SparkColourPickerUtils.h"
-@interface PXLPrefs:Turann{BOOL SingleColorMode;}
+@interface PXLPrefs:Turann{BOOL CustomTicks;}
 @property (nonatomic, retain) UIBarButtonItem *respringApplyButton;
 @property (nonatomic, retain) UIBarButtonItem *respringConfirmButton;
+@property (nonatomic, strong) NSArray *customColorIDS;
 @end
